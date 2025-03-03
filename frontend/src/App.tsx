daccebeb2a245073e4c661b4cf2c928df442820a
Pedro V1dal <pedro.vidal2608@gmail.com>
2025-03-03 16:15:17 -0300

    finish the header of the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Logo from "./assets/Logo.svg";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <>
+      <div className="header">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+      </div>
+      <div className="wrapper">
+        <h1>Conteúdo da página</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
