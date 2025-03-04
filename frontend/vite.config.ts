e882c97250683e1a275312e1e49ca3c799f4b385
Pedro V1dal <pedro.vidal2608@gmail.com>
2025-03-04 12:15:30 -0300

    vite configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const env = loadEnv("", process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+    build: {
+      outDir: "build",
+      target: "esnext",
+    },
+  };
 });
